d8b92ae865aaeace90ab1d702e7484569e9a8dc8
sankarsharn <sankarsharnrastogi@gmail.com>
2025-04-13 15:33:12 +0530

    Added the doctor model

---
+++ src/models/doctor.model.ts
@@ -0,0 +1,44 @@
+import Specialities from '@/app/components/Specialities';
+import mongoose from 'mongoose';
+
+const doctorSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    degree:{
+        type:String,
+        required:true
+    },
+    specialization:{
+        type:String,
+        required:true
+    },
+    city:{
+        type:String,
+        required:true
+    },
+    experience:{
+        type:Number,
+        required:true
+    },
+    license:{
+        type:String,
+        required:true
+    },
+    category:{
+        type:Number,
+        default: 0,
+    }
+} , {timestamps:true});
+
+export default mongoose.models.Doctor || mongoose.model('Doctor', doctorSchema);
