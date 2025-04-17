3cb5ba7e72f4ddcfb604728f92755ea4a15bd25a
sankarsharn <sankarsharnrastogi@gmail.com>
2025-04-17 12:05:12 +0530

    Added appointment Schema

---
+++ src/models/appointmentRequest.model.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const appointmentRequestSchema = new mongoose.Schema({
+    doctor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Doctor',
+        required: true,
+    },
+    patient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'accepted', 'rejected'],
+        default: 'pending',
+    }
+}, {timestamps: true,})
